e9ccf8f485e1263211dacc0bc955c094675a0c62
AshEllis52 <83059758+AshEllis52@users.noreply.github.com>
2021-05-15 13:10:43 +0100

    Create views.py

---
+++ views.py
@@ -0,0 +1,15 @@
+from plotly.offline import plot
+from plotly.graph_objs import Scatter
+from flask import Markup
+
+@app.route('/results', methods=['GET', 'POST'])
+def results():
+    error = None
+    if request.method == 'POST':
+        my_plot_div = plot([Scatter(x=[1, 2, 3], y=[3, 1, 6])], output_type='div')
+        return render_template('results.html',
+                               div_placeholder=Markup(my_plot_div)
+                              )
+    # If user tries to get to page directly, redirect to submission page
+    elif request.method == "GET":
+        return redirect(url_for('submission', error=error))
