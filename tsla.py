8edaeb908893c1ff827b39581732bcad7b2d99e9
AshEllis52 <83059758+AshEllis52@users.noreply.github.com>
2021-05-26 17:14:31 +0100

    Update tsla.py

--- tsla.py
+++ tsla.py
@@ -26,7 +26,7 @@ def hello(): # Name of the method
 @app.route("/aapl") #add
 def aapl():
  cur = conn2.cursor()
- cur.execute('''Select * FROM dbo.AAPL''')
+ cur.execute('''Select * Columns FROM AAPL''')
  rv = cur.fetchall()
  Results = []
  for row in rv:
