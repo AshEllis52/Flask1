e79006749adb5e69a75b7c2b0265616aedd3cc89
AshEllis52 <83059758+AshEllis52@users.noreply.github.com>
2021-05-26 14:52:39 +0100

    Update panda.py

--- panda.py
+++ panda.py
@@ -1,49 +1,13 @@
 from flask import Flask
 from flask import request
-from flask_cors import CORS
-import json
-import pypyodbc
 app = Flask(__name__)
-CORS(app)
-
-with open(".pw") as f:
-  password = f.read()
-
-
-conn2 = pypyodbc.connect('Driver={ODBC Driver 17 for SQL Server};'
-'Server=ashdb.dbsprojects.ie;'
-'Database=Stocks;'
-'encrypt=yes;'
-'TrustServerCertificate=yes;'
-'UID=sa;'
-'PWD='+password,autocommit = True)
-
-cur = conn2.cursor()
-
-
-@app.route("/tsla") #add
-def add():
- cur = conn2.cursor()
- cur.execute('''Select * FROM TSLA''')
- rv = cur.fetchall()
- Results = ()
- for row in rv:
-  Results = []
-  Results['Date'] = row[0].replace('/n',' ')
-  Results['Open'] = row[1]
-  Reults['High'] = row[2]
-  Results['Low'] = row[3]
-  Reults['Close'] = row[4]
-  Results['Adj Close'] = row[5]
-  Results['Volume'] = row[6]
-  Results.append(Results)
- response={'Results':Results, 'count':len(Results)}
- ret=app.response_class(
-  response=json.dumps(response),
-  status=200,
-  mimetype='application/json'
- )
-
+@app.route("/")#URL leading to method
+def hello(): # Name of the method
+ return("Hello World!")
+@app.route("/greetme")#different URL
+def helloall(): # different method name
+ name = request.args.get('name')#retrieve GET parameters
+ return("Hello {}!".format(name))#Python’s string.format
 if __name__ == "__main__":
  app.run(host='0.0.0.0',port='8080', ssl_context=('cert.pem', 'privkey.pem')) #Run the flask app at port 8080
 
