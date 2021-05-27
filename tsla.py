5024cecb8ee14415291acef6a10b34cb24938895
AshEllis52 <83059758+AshEllis52@users.noreply.github.com>
2021-05-27 15:38:10 +0100

    Update tsla.py

--- tsla.py
+++ tsla.py
@@ -21,7 +21,7 @@ cur = conn2.cursor()
 
 @app.route("/")#URL leading to method
 def hello(): # Name of the method
- return("Hello World!")
+ return("Do you like Stocks?")
 
 @app.route("/aapl") 
 def aapl():
@@ -69,6 +69,79 @@ def tsla():
   status=200,
   mimetype='application/json'
  )
+  return ret
+
+@app.route("/ba") 
+def ba():
+ cur = conn2.cursor()
+ cur.execute('''Select * FROM BA''')
+ rv = cur.fetchall()
+ Results = []
+ for row in rv:
+  Result = {}
+  Result['Date'] = row[0].replace('\n',' ')
+  Result['Open'] = row[1]
+  Result['High'] = row[2]
+  Result['Low'] = row[3]
+  Result['Close'] = row[4]
+  Result['Adj Close'] = row[5]
+  Result['Volume'] = row[6]
+  Results.append(Result)
+ response={'Results':Results, 'count':len(Results)}
+ ret=app.response_class(
+  response=json.dumps(response),
+  status=200,
+  mimetype='application/json'
+ )
  return ret
+
+@app.route("/gold") 
+def gold():
+ cur = conn2.cursor()
+ cur.execute('''Select * FROM GOLD''')
+ rv = cur.fetchall()
+ Results = []
+ for row in rv:
+  Result = {}
+  Result['Date'] = row[0].replace('\n',' ')
+  Result['Open'] = row[1]
+  Result['High'] = row[2]
+  Result['Low'] = row[3]
+  Result['Close'] = row[4]
+  Result['Adj Close'] = row[5]
+  Result['Volume'] = row[6]
+  Results.append(Result)
+ response={'Results':Results, 'count':len(Results)}
+ ret=app.response_class(
+  response=json.dumps(response),
+  status=200,
+  mimetype='application/json'
+ )
+ return ret
+
+@app.route("/dog") 
+def doge():
+ cur = conn2.cursor()
+ cur.execute('''Select * FROM DOGE''')
+ rv = cur.fetchall()
+ Results = []
+ for row in rv:
+  Result = {}
+  Result['Date'] = row[0].replace('\n',' ')
+  Result['Open'] = row[1]
+  Result['High'] = row[2]
+  Result['Low'] = row[3]
+  Result['Close'] = row[4]
+  Result['Adj Close'] = row[5]
+  Result['Volume'] = row[6]
+  Results.append(Result)
+ response={'Results':Results, 'count':len(Results)}
+ ret=app.response_class(
+  response=json.dumps(response),
+  status=200,
+  mimetype='application/json'
+ )
+ return ret
+
 if __name__ == "__main__":
   app.run(host='0.0.0.0',port='8080', ssl_context=('cert.pem', 'privkey.pem')) 
