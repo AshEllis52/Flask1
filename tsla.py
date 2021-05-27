c3b1b7e66fd91ec823b999884a10b090f76446f2
AshEllis52 <83059758+AshEllis52@users.noreply.github.com>
2021-05-27 14:00:19 +0100

    Update tsla.py

--- tsla.py
+++ tsla.py
@@ -61,8 +61,8 @@ def tsla():
   Results['Low'] = row[3]
   Results['Close'] = row[4]
   Results['Adj Close'] = row[5]
-  #Results['Volume'] = row[6]
-  #Results.append(Result)
+  Results['Volume'] = row[6]
+  Results.append(Result)
  response={'Results':Results, 'count':len(Results)}
  ret=app.response_class(
   response=json.dumps(response),
