32fc9dc676b9850780384c54e570fba45d8bf1b7
AshEllis52 <83059758+AshEllis52@users.noreply.github.com>
2021-05-28 09:48:49 +0100

    Update tsla.py

--- tsla.py
+++ tsla.py
@@ -5,11 +5,10 @@ import json
 import pypyodbc
 app = Flask(__name__)
 CORS(app)
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as pyplot
 from io import BytesIO
 import numpy as np
-import cgi
-from io import BytesIO
+
 
 with open(".pw") as f:
   password = f.read()
@@ -23,42 +22,40 @@ conn2 = pypyodbc.connect('Driver={ODBC Driver 17 for SQL Server};'
 'PWD='+password,autocommit = True)
 
 cur = conn2.cursor()
-self = x
+
+
 
 @app.route("/")#URL leading to method
-def hello(self): 
-    np.random.seed(4500)
+np.random.seed(4500)
 
-    mean = 150
-    sd = 10
-    n = 1000
+mean = 150
+sd = 10
+n = 1000
 
-    heights = np.random.normal(mean, sd, n)
+heights = np.random.normal(mean, sd, n)
 
-    density = False
+density = False
 
-    hist, bin_edges = np.histogram(heights, bins=50, density = density)
+hist, bin_edges = np.histogram(heights, bins=50, density = density)
 
-    bin_width = bin_edges[2] - bin_edges[1]
-    print("Bin Width =", bin_width)
+bin_width = bin_edges[2] - bin_edges[1]
+print("Bin Width =", bin_width)
 
-    plt.figure()
-    plt.plot(bin_edges[:-1], hist, color="green", label="heights histogram")
-    plt.xlabel("Height")
-    plt.ylabel("Frequency")
-    plt.grid()
-    plt.legend()
-    plt.title("Histogram/Density Functions of heights of college students")
-    plt.show()
-    fig = plt.figure()
-    #plt.plot(range(10))
-    figdata = BytesIO()
-    fig.savefig(figdata, format='png')
+pyplot.figure()
+pyplot.plot(bin_edges[:-1], hist, color="green", label="heights histogram")
+pyplot.xlabel("Height")
+pyplot.ylabel("Frequency")
+pyplot.grid()
+pyplot.legend()
+pyplot.title("Histogram/Density Functions of heights of college students")
+pyplot.show()
 
-    self.set_header('Content-Type', 'image/png')
-    self.write(figdata.getvalue())
-   
-    return(figdata.getvalue())
+format = "png"
+sio = BytesIO()
+pyplot.savefig(sio, format=format)
+print ("Content-Type: image/%s\n" % format)
+#msvcrt.setmode(sys.stdout.fileno(), os.O_BINARY) # Needed this on windows, IIS
+sys.stdout.write(sio.getvalue())
 
 
 if __name__ == "__main__":
