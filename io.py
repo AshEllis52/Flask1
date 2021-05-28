8732183df6f9889d1418f54209471f5a961cf781
AshEllis52 <83059758+AshEllis52@users.noreply.github.com>
2021-05-28 09:03:18 +0100

    Update io.py

--- io.py
+++ io.py
@@ -1,16 +1,34 @@
-import io
-from matplotlib.figure import Figure     
-from matplotlib import pyplot as plt                 
+import matplotlib.pyplot as plt
+from io import BytesIO
+import numpy as np
+import cgi
+from io import BytesIO
 
-fig = Figure(figsize=[4,4])                               
-ax = fig.add_axes([.1,.1,.8,.8])                          
-ax.scatter([1,2], [3,4])                                  
+np.random.seed(4500)
 
-buf = io.BytesIO()
-fig.savefig(buf, format='png')
-plt.close(fig)
-data=buf.getvalue()
+mean = 150
+sd = 10
+n = 1000
 
-# In my case I would have used Django for the webpage
-response = HttpResponse(data, content_type='image/png')
- return response
+heights = np.random.normal(mean, sd, n)
+
+density = False
+
+hist, bin_edges = np.histogram(heights, bins=50, density = density)
+
+bin_width = bin_edges[2] - bin_edges[1]
+print("Bin Width =", bin_width)
+
+plt.figure()
+plt.plot(bin_edges[:-1], hist, color="green", label="heights histogram")
+plt.xlabel("Height")
+plt.ylabel("Frequency")
+plt.grid()
+plt.legend()
+plt.title("Histogram/Density Functions of heights of college students")
+#plt.show()
+fig = plt.figure()
+#plt.plot(range(10))
+figdata = BytesIO()
+fig.savefig(figdata, format='png')
+
