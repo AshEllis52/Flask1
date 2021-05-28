44e627b08bd8ab2d5c9f5283faf3165fad21c88b
AshEllis52 <83059758+AshEllis52@users.noreply.github.com>
2021-05-28 09:28:52 +0100

    Update tsla.py

--- tsla.py
+++ tsla.py
@@ -5,6 +5,11 @@ import json
 import pypyodbc
 app = Flask(__name__)
 CORS(app)
+import matplotlib.pyplot as plt
+from io import BytesIO
+import numpy as np
+import cgi
+from io import BytesIO
 
 with open(".pw") as f:
   password = f.read()
@@ -20,8 +25,39 @@ conn2 = pypyodbc.connect('Driver={ODBC Driver 17 for SQL Server};'
 cur = conn2.cursor()
 
 @app.route("/")#URL leading to method
-def hello(): # Name of the method
- return("Do you like Stocks?")
+def hello(): 
+    np.random.seed(4500)
+
+    mean = 150
+    sd = 10
+    n = 1000
+
+    heights = np.random.normal(mean, sd, n)
+
+    density = False
+
+    hist, bin_edges = np.histogram(heights, bins=50, density = density)
+
+    bin_width = bin_edges[2] - bin_edges[1]
+    print("Bin Width =", bin_width)
+
+    plt.figure()
+    plt.plot(bin_edges[:-1], hist, color="green", label="heights histogram")
+    plt.xlabel("Height")
+    plt.ylabel("Frequency")
+    plt.grid()
+    plt.legend()
+    plt.title("Histogram/Density Functions of heights of college students")
+    plt.show()
+    fig = plt.figure()
+    #plt.plot(range(10))
+    figdata = BytesIO()
+    fig.savefig(figdata, format='png')
+
+
+   
+    return(self.set_header('Content-Type', 'image/png')
+        self.write(figdata.getvalue()))
 
 @app.route("/aapl") 
 def aapl():
