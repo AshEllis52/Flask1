e3e74a0907233be171ec54ccc415851cc175dd4a
AshEllis52 <83059758+AshEllis52@users.noreply.github.com>
2021-05-28 08:49:45 +0100

    Create io.py

---
+++ io.py
@@ -0,0 +1,16 @@
+import io
+from matplotlib.figure import Figure     
+from matplotlib import pyplot as plt                 
+
+fig = Figure(figsize=[4,4])                               
+ax = fig.add_axes([.1,.1,.8,.8])                          
+ax.scatter([1,2], [3,4])                                  
+
+buf = io.BytesIO()
+fig.savefig(buf, format='png')
+plt.close(fig)
+data=buf.getvalue()
+
+# In my case I would have used Django for the webpage
+response = HttpResponse(data, content_type='image/png')
+return response
